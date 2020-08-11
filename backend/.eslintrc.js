5d7d26a027af05b92d2607bd380d0f1aa663d0c2
ryuhangyeong <hangyeong.ryu@gmail.com>
2020-08-11 17:58:48 +0900

    [수정] 정적 파일 경로 수정 및 lint 설정 변경

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,17 +1,14 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "env": {
-        "commonjs": true,
-        "es6": true
+    parser: "babel-eslint",
+    env: {
+        commonjs: true,
+        es6: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:prettier/recommended"
-    ],
-    "parserOptions": {
-        "sourceType": "module"
+    extends: ["eslint:recommended", "plugin:prettier/recommended"],
+    parserOptions: {
+        sourceType: "module",
     },
-    "rules": {
-        "prettier/prettier": "error"
-    }
+    rules: {
+        "prettier/prettier": "error",
+    },
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "html");
 app.set("views", __dirname + "/dist");
 app.engine("html", require("ejs").renderFile);
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("/", (req, res) => res.render("index"));
 app.listen(PORT, () => console.log(`Todo App Running PORT: ${PORT}`));

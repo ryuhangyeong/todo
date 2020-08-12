5f7191ef2d1413d43d2869b271c305dd73afac29
ryuhangyeong <hangyeong.ryu@gmail.com>
2020-08-12 13:43:26 +0900

    [수정] elsint 설정 변경 및 불필요 변수 삭제

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
     parser: "babel-eslint",
     env: {
-        commonjs: true,
         es6: true,
+        node: true
     },
     extends: ["eslint:recommended", "plugin:prettier/recommended"],
     parserOptions: {
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,16 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
-import mysql from "mysql2/promise";
 
 dotenv.config();
 
 const app = express();
-const { PORT, DB, DB_HOST, DB_USER, DB_PASS } = process.env;
-
-let pool = mysql.createPool({
-    database: DB,
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASS,
-});
+const { PORT } = process.env;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', "html");
+app.set("view engine", "html");
 app.set("views", __dirname + "/dist");
 app.engine("html", require("ejs").renderFile);
 app.use(express.static(path.join(__dirname, "../dist")));

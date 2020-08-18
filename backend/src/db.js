f157b66b6ad7dfcf878618c35c483006d1244cd3
ryuhangyeong <hangyeong.ryu@gmail.com>
2020-08-18 14:08:18 +0900

    [리팩토링] db 연결 함수 추가 및 id param route chain

---
+++ backend/src/db.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import mysql from "mysql2/promise";
+
+dotenv.config();
+const { DB, DB_HOST, DB_USER, DB_PASS } = process.env;
+
+const pool = mysql.createPool({
+    database: DB,
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASS,
+    connectionLimit: 50,
+});
+
+export default async (query, params = []) => {
+    try {
+        const connection = await pool.getConnection(async (_) => _);
+        const data = await connection.query(query, params);
+        connection.release();
+        return data;
+    } catch (e) {
+        throw e;
+    }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,12 @@
 import path from "path";
 import dotenv from "dotenv";
 import express from "express";
-import mysql from "mysql2/promise";
+import db from "./db";
 
 dotenv.config();
 
 const app = express();
-const { PORT, DB, DB_HOST, DB_USER, DB_PASS } = process.env;
+const { PORT } = process.env;
 const API_ENDPOINT = "/api/todo";
 
 app.set("view engine", "html");
@@ -17,24 +17,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../dist")));
 
-const pool = mysql.createPool({
-    database: DB,
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASS,
-    connectionLimit: 50,
-});
-
 app.get("/", (_, res) => res.render("index"));
 
 app.route(API_ENDPOINT)
     .get(async (req, res) => {
         try {
-            const connection = await pool.getConnection(async (_) => _);
-            const [data] = await connection.query(
+            const [data] = await db(
                 "SELECT id, title, completed from list where deleteFlag = 0 order by id desc"
             );
-            connection.release();
             res.status(200).json(data);
         } catch (e) {
             res.status(400).json(e);
@@ -44,63 +34,48 @@ app.route(API_ENDPOINT)
         const { title } = req.body;
 
         try {
-            const connection = await pool.getConnection(async (_) => _);
-            const [
-                data,
-            ] = await connection.query("INSERT INTO list(title) VALUES (?)", [
+            const [data] = await db("INSERT INTO list(title) VALUES (?)", [
                 title,
             ]);
-            connection.release();
             res.status(200).json(data);
         } catch (e) {
             res.status(400).json(e);
         }
     });
 
-app.put(`${API_ENDPOINT}/:id`, async (req, res) => {
-    const { id } = req.params;
+app.route(`${API_ENDPOINT}/:id`)
+    .put(async (req, res) => {
+        const { id } = req.params;
 
-    try {
-        const connection = await pool.getConnection(async (_) => _);
+        try {
+            const [data] = await db("SELECT completed from list WHERE id = ?", [
+                id,
+            ]);
 
-        const [
-            data,
-        ] = await connection.query("SELECT completed from list WHERE id = ?", [
-            id,
-        ]);
+            const [
+                updateData,
+            ] = await db("UPDATE list SET completed = ? WHERE id = ?", [
+                data[0].completed ? 0 : 1,
+                id,
+            ]);
 
-        const [
-            updateData,
-        ] = await connection.query(
-            "UPDATE list SET completed = ? WHERE id = ?",
-            [data[0].completed ? 0 : 1, id]
-        );
+            res.status(200).json(updateData);
+        } catch (e) {
+            res.status(400).json(e);
+        }
+    })
+    .delete(async (req, res) => {
+        const { id } = req.params;
 
-        connection.release();
-        res.status(200).json(updateData);
-    } catch (e) {
-        res.status(400).json(e);
-    }
-});
+        try {
+            const [
+                data,
+            ] = await db("UPDATE list SET deleteFlag = 1 WHERE id = ?", [id]);
 
-app.delete(`${API_ENDPOINT}/:id`, async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        const connection = await pool.getConnection(async (_) => _);
-
-        const [
-            data,
-        ] = await connection.query(
-            "UPDATE list SET deleteFlag = 1 WHERE id = ?",
-            [id]
-        );
-
-        connection.release();
-        res.status(200).json(data);
-    } catch (e) {
-        res.status(400).json(e);
-    }
-});
+            res.status(200).json(data);
+        } catch (e) {
+            res.status(400).json(e);
+        }
+    });
 
 app.listen(PORT, () => console.log(`Todo App Running PORT: ${PORT}`));

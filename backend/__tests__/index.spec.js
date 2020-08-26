0ecdd5dd2d692fa40774fa60aef610d2dc585420
ryuhangyeong <hangyeong.ryu@gmail.com>
2020-08-26 18:07:09 +0900

    [수정] 테스트 예외 사항

--- backend/__tests__/index.spec.js
+++ backend/__tests__/index.spec.js
@@ -5,6 +5,8 @@ const API_ENDPOINT = "/api/todo";
 
 /*
     @description 좋은 테스트 코드 작성을 위해 고민중에 있습니다.
+    @notice .env 및 mysql 등의 의존성 때문에 테스트가 깨진다.
+    @todo 이 부분을 mock으로 대체해야한다.
  */
 describe.skip(`${API_ENDPOINT}`, () => {
     it("GET/", async (done) => {
